1724ddce8846034f3531bf439796e47bcb9492b1
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-12-04 18:26:40 -0300

    move canvas to index.html

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import './style.css'
 
-import {loadScene} from './firstScene'
+import {loadScene} from './babylon'
 
 const app = document.querySelector<HTMLDivElement>('#app')
 
 if (app) {
-  app.innerHTML = `
-    <canvas id="renderCanvas" touch-action="none"></canvas>
-  `
   loadScene()
 }
